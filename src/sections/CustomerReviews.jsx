fec6435198b41b21b124e0ab98c81786afd963a2
Lucas Morató Nieto <99823243+lucasmoratonieto@users.noreply.github.com>
2024-01-24 15:30:17 +0000

    TabS7

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,12 +1,12 @@
 const CustomerReviews = () => {
   return (
-  <section className=" flex align-middle justify-center">
-    <div className=" ">
+  <section className="flex flex-col align-middle justify-center text-center gap-5 max-container">
+    <div className=" text-4xl font-bold font-palanquin">
       What Our <span className=" text-coral-red">Customers</span> Say?
     </div>
-    <div>
-    Hear genuine stories from our satisfied customers about their exceptional experiences with us.
-    </div>
+      <div className=" m-auto mt-4 max-w-lg  text-center info-text ">
+        Hear genuine stories from our satisfied customers about their exceptional experiences with us.
+      </div>
 
   </section>
 )};
